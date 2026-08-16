90a82d9dddd3186bdc626c70b2a35a06fe5d1b21
Karsilama <jesus.ac@gmail.com>
2026-08-16 12:18:49 +0200

    chore: http resource

--- apps/catalog/src/app/page/product/all/product-all.ts
+++ apps/catalog/src/app/page/product/all/product-all.ts
@@ -1,15 +1,21 @@
+import { httpResource } from "@angular/common/http";
 import { Component, inject, signal } from "@angular/core";
+import { MatListModule } from "@angular/material/list";
 import { Router } from "@angular/router";
+import { ProductResource } from "../ product.model";
 import { ButtonFilled } from "../../../core/button/button-filled";
 import { ButtonDefinitions } from "../../../core/form/definitions/button-definition";
+import { ProductAll } from "../product.constant";
 
 @Component({
   selector: "product-all-page",
   templateUrl: "./product-all.html",
-  imports: [ButtonFilled],
+  imports: [ButtonFilled, MatListModule],
 })
 export class ProductAllPage {
   public router = inject(Router);
+
+  public productAll = httpResource<ProductResource>(() => ProductAll);
 
   public navigateButton = signal<ButtonDefinitions.Filled>({
     innerHtml: "Navigate to product id 1",
---
+++ apps/catalog/src/app/page/product/product.constant.ts
@@ -0,0 +1,3 @@
+export const ProductAll = {
+  url: `https://dummyjson.com/products`,
+};
